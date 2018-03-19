8b413afe183fec06935bb05beea3ed0ac4bd9203
mattgagne <matt.gagne@gmail.com>
2018-03-19 18:36:15 -0400

    add movie and change some constants

--- SimulatorScriptOfficial.m
+++ SimulatorScriptOfficial.m
@@ -16,13 +16,13 @@ U=tau_0;
 q=qout(end,[1,3])';
     
 % Additions for testing
-y_des = [deg2rad(-50) deg2rad(0)]';
-ts = 0:0.001:5;
+y_des = [deg2rad(-85) deg2rad(0)]';
+ts = 0:0.001:3;
 e_prev = q - y_des;
 % End of additions
 
 
-for t=0.001:0.001:5
+for t=0.001:0.001:3
    t
    %check if robot meets requirements
 
@@ -37,11 +37,39 @@ for t=0.001:0.001:5
  
  %calculate energy/time, etc...
 
- % Additions for visualization
- figure();
+ %% Additions for visualization
+ figure(1);
  subplot(2,1,1);
  plot(ts, q(1,:));
  subplot(2,1,2);
  plot(ts, q(2,:));
+ 
+ 
+%% Create AVI object
+makemovie = 1;
+if(makemovie)
+    vidObj = VideoWriter('ekf.avi');
+    vidObj.Quality = 100;
+    vidObj.FrameRate = 8;
+    open(vidObj);
+end
+
+for i = 1:50:size(q,2)
+    figure(2);clf; hold on;
+    x1 = l1*cos(q(1,i));
+    y1 = l1*sin(q(1,i));
+    x2 = l1*cos(q(1,i)) + l2*cos(q(1,i) + q(2,i));
+    y2 = l1*sin(q(1,i)) + l2*sin(q(1,i) + q(2,i));
+    plot([0,x1],[0,y1],'r');
+    hold on;
+    plot([x1,x2],[y1,y2],'b');
+    hold on;
+    scatter(my_goalpoints(:,1),my_goalpoints(:,2),15,'filled');
+    xlim([-0.22,0.22]);
+    ylim([-0.22,0.22]);
+    if (makemovie) writeVideo(vidObj, getframe(gca)); end
+end
+
  % End of additions
+ 
 
--- constants.m
+++ constants.m
@@ -1,9 +1,9 @@
 %put constant values in this file%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%You NEED these constants%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-c1=1;%link 1 friction coeffecient
-c2=1;%link 2 friction coeffecient
-l1=1; %link 1 length
-l2=1; %link 2 length
+c1=0.001;%link 1 friction coeffecient
+c2=0.001;%link 2 friction coeffecient
+l1=0.2; %link 1 length
+l2=0.1; %link 2 length
 m1=1;%link 1 mass
 m2=1;%link 2 mass
 g=3.7;%acceleration due to gravity m/s^2 on mars
@@ -15,4 +15,6 @@ my_angle_vector=[0 0]';
 my_state_estimate_vector=[0 0 0 0]';
 my_some_variable_a=0;
 my_some_variable_b=0;
+my_goalpoints = [0.1 0.2; 0.2 0.2; 0.2 0.1; 0.1 0.1];
+%my_waypoints;
 deltaXe_prev = x_0;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
