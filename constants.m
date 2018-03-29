eed65ae09a6da3bdbcd7dcbdf505c4188992607b
Nick Manna <manna422@gmail.com>
2018-03-29 17:08:23 -0400

    WIP3

--- constants.m
+++ constants.m
@@ -4,10 +4,10 @@ density = 1.4;
 length_total = 0.75/density;
 
 
-c1=6;%8;%link 1 friction coeffecient
-c2=6;%6;%link 2 friction coeffecient
-l1=1/2*length_total; %link 1 length
-l2=1/2*length_total; %link 2 length
+c1=6.5;%8;%link 1 friction coeffecient
+c2=6.5;%6;%link 2 friction coeffecient
+l1=0.539*length_total; %link 1 length
+l2=length_total - l1; %link 2 length
 m1=density*l1;%link 1 mass
 m2=density*l2;%link 2 mass
 g=3.7;%acceleration due to gravity m/s^2 on mars
@@ -15,10 +15,16 @@ measurement_std_dev = deg2rad(0.333);
 pA = [0.1 0.2];
 pB = [0.2 0.2]; 
 pC = [0.2 0.1]; pD = [0.1 0.1];
-num_waypts = 2;
-my_waypts_xy = [logspace( log10(pA(1)), log10(pB(1)), num_waypts ) pB(1) logspace( log10(pB(1)), log10(pC(1)), num_waypts ) pC(1) logspace( log10(pC(1)), log10(pD(1)), num_waypts ) pD(1) logspace( log10(pD(1)), log10(pA(1)), num_waypts ) pA(1);
-                logspace( log10(pA(2)), log10(pB(2)), num_waypts ) pB(2) logspace( log10(pB(2)), log10(pC(2)), num_waypts ) pC(2) logspace( log10(pC(2)), log10(pD(2)), num_waypts ) pD(2) logspace( log10(pD(2)), log10(pA(2)), num_waypts ) pA(2)];
-for i = 1:size(my_waypts_xy,2)
+num_waypts = 10;
+
+my_waypts_xy = [linspace( pA(1), pB(1), num_waypts ) pB(1) linspace( pB(1), pC(1), num_waypts ) pC(1) linspace( pC(1), pD(1), num_waypts ) pD(1) linspace( pD(1), pA(1), num_waypts ) pA(1);
+                linspace( pA(2), pB(2), num_waypts ) pB(2) linspace( pB(2), pC(2), num_waypts ) pC(2) linspace( pC(2), pD(2), num_waypts ) pD(2) linspace( pD(2), pA(2), num_waypts ) pA(2)];
+
+
+% my_waypts_xy = [logspace( log10(pA(1)), log10(pB(1)), num_waypts ) pB(1) logspace( log10(pB(1)), log10(pC(1)), num_waypts ) pC(1) logspace( log10(pC(1)), log10(pD(1)), num_waypts ) pD(1) logspace( log10(pD(1)), log10(pA(1)), num_waypts ) pA(1);
+%                 logspace( log10(pA(2)), log10(pB(2)), num_waypts ) pB(2) logspace( log10(pB(2)), log10(pC(2)), num_waypts ) pC(2) logspace( log10(pC(2)), log10(pD(2)), num_waypts ) pD(2) logspace( log10(pD(2)), log10(pA(2)), num_waypts ) pA(2)];
+
+            for i = 1:size(my_waypts_xy,2)
     [my_waypts_ang(1,i) my_waypts_ang(2,i)] = getAngle(my_waypts_xy(1,i), my_waypts_xy(2,i), l1, l2);
 end
 x_0=[my_waypts_ang(1,1),0,my_waypts_ang(2,1),0]';%x_0=[q1_0,q1dot_0,q2_0,q2dot_0] initial conditions for the robot
